7a38d9a7cd951bf70dbfb2c48b8766c8dc2b616e
Katarina Luise Matthes <kmatth@iem.uzh.ch>
2023-02-02 15:05:32 +0100

    start

---
+++ master.R
@@ -0,0 +1,8 @@
+.libPaths(c("H:/Documents/R/win-library/4.1", "C:/Program Files/R/R-4.2.2/library"))
+
+library(tidyverse)
+library(openxlsx)
+library(lubridate)
+# library(nlme)
+library(lme4)
+library(lmerTest)
